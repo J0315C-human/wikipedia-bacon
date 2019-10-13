dd48a233965a599db42a880645af84b4201fb875
J0315C-human <foibles@gmail.com>
2019-10-13 15:46:12 -0500

    utils for web crawling

---
+++ src/utils.ts
@@ -0,0 +1,54 @@
+import * as cheerio from 'cheerio';
+import axios from 'axios';
+import * as URI from 'urijs';
+import { urlBlacklist } from './constants';
+import { Page } from './PageGraph';
+
+const _wikiLinkSelectors = ['#mw-content-text a', '.infobox a'];
+
+function checkIsValidLink(url: string) {
+  // turn down links to same page and special wikipedia pages
+  if (
+    url.startsWith('#') ||
+    urlBlacklist.some(urlPart => url.includes(urlPart))
+  ) {
+    return false;
+  }
+  const uri = URI(url);
+  const domain = uri.domain();
+  if (domain === 'wikipedia.org' || (!domain && url.startsWith('/'))) {
+    return true;
+  } else return false;
+}
+
+function getValidLinks(html: string) {
+  const links = [] as string[];
+  const $ = cheerio.load(html);
+  _wikiLinkSelectors.forEach(selector => {
+    const elements = $(selector);
+    elements.each((_, el) => {
+      const { href } = el.attribs;
+      if (href) {
+        const isValid = checkIsValidLink(href);
+        if (isValid) {
+          if (href.startsWith('/')) {
+            links.push('https://en.wikipedia.org' + href);
+          } else {
+            links.push(href);
+          }
+        }
+      }
+    });
+  });
+  // filter out duplicates
+  return links.filter((link, idx) => links.indexOf(link) === idx);
+}
+
+export async function getPageFromUrl(url: string): Promise<Page> {
+  const response = await axios(url);
+  const links = getValidLinks(response.data);
+  return {
+    id: url,
+    outgoingLinks: links
+  };
+}
